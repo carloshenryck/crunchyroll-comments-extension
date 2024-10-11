42af7159ee6b4bec93becd362ea8d9ef14422bdc
Carlos Henryck <ryck302@gmail.com>
2024-10-11 20:17:28 -0300

    feat: create comments component

---
+++ src/components/Comments/index.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+import "./style.css";
+
+export default function Comments() {
+  useEffect(() => {}, []);
+
+  return (
+    <div className="comments-wrapper">
+      <h2>Comentários</h2>
+    </div>
+  );
+}
--- src/content.tsx
+++ src/content.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { waitForElement } from "./utils/waitForElement";
 import { addBodyObserver } from "./utils/observerBody";
 import AddComments from "./components/AddComments";
+import Comments from "./components/Comments";
 
 let currentUrl: string = "";
 const rootId = "crx-root";
@@ -15,6 +16,10 @@ const observerCallback = () => {
 
   if (updatedUrl.startsWith("https://www.crunchyroll.com/pt-br/series/")) {
     addSeasonsContentScript();
+  }
+
+  if (updatedUrl.startsWith("https://www.crunchyroll.com/pt-br/watch/")) {
+    addCommentsContentScript();
   }
 
   currentUrl = updatedUrl;
@@ -36,4 +41,15 @@ async function addSeasonsContentScript() {
 
   seasonSelect.insertAdjacentElement("afterend", root);
   createRoot(document.getElementById(rootId)!).render(<AddComments />);
+}
+
+async function addCommentsContentScript() {
+  const root = document.createElement("div");
+  root.setAttribute("id", rootId);
+
+  const bodyWrapper = await waitForElement(".body-wrapper");
+  if (!bodyWrapper) return;
+
+  bodyWrapper.insertAdjacentElement("afterend", root);
+  createRoot(document.getElementById(rootId)!).render(<Comments />);
 }
